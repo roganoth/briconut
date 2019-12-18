2404d35c0eebf01df434b469b50a3f094b8b7cff
dan <chronos937@gmail.com>
2019-12-17 20:39:00 -0500

    wrote orm.js connection.js and the controller

---
+++ models/hrApp.js
@@ -0,0 +1,26 @@
+var orm = require("../config/orm.js");
+
+var employees = {
+    selectAll: function (cb) {
+        orm.selectAll("employees", function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("employees", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("employees", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("employees", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = employees;
