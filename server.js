cda4bef9c5c8b78092fb2e421905231ae8956043
jNak <juliannakar@yahoo.com>
2019-12-19 18:56:48 -0500

    schema & add html

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var path = require("path")
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controller/hrApp-controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Server listening on : http://localhost:" + PORT);
+});
