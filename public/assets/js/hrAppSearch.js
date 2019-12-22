600aa943678c5e210bd2e749050986c40519a87a
dan <chronos937@gmail.com>
2019-12-21 21:48:04 -0500

    added code to the if condition to allow searching with numbers for phone number or email sorting. kept search button code in but commented out in case we need it for later development but if not should be removed before completion

--- public/assets/js/hrAppSearch.js
+++ public/assets/js/hrAppSearch.js
@@ -21,9 +21,36 @@ $(document).ready(function () {
     });
     // $(document).on("click", "#search", function (event) {
     //     event.preventDefault();
+    //     var column = $("#col").val().trim();
+    //     var colVal = $("#search-bar").val().trim();
+    //     $("#employeeResult").empty();
+    //     $.ajax("/employees/" + column + "/" + colVal, {
+    //         type: "GET"
+    //     }).then(function (data) {
+    //         var emps = data.employees;
+    //         var len = data.employees.length;
+
+    //         var emps_elem = $("#employeeResult");
+    //         for (i = 0; i < len; i++) {
+    //             emps_elem.append(
+    //                 "<li><p>" +
+    //                 emps[i].first_name + " " +
+    //                 emps[i].last_name + " | " +
+    //                 emps[i].position + " | " +
+    //                 moment(emps[i].hire_date).format('LL') + " | " +
+    //                 emps[i].email + " | " +
+    //                 "<button id='info'>Get More Info</button>" +
+    //                 "</p></li><hr>"
+    //             );
+    //         };
+
+    //     });
+    // });
+
+
     $("#search-bar").keyup(function (event) {
         console.log(event.keyCode);
-        if (event.keyCode >= 65 && event.keyCode <= 90) {
+        if (event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode >= 48 && event.keyCode <= 57) {
             var column = $("#col").val().trim();
             var colVal = $("#search-bar").val().trim();
             $("#employeeResult").empty();
